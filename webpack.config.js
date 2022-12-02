854ec17831ee49b7ceb5e1c800e6f665c4a60f91
Joel Wong <45819490+joelwongjy@users.noreply.github.com>
2022-12-02 04:39:20 -0800

    feat: initialize webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] },
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve("./src"), path.resolve("./node_modules")],
+    extensions: ["", ".js", ".ts", ".tsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
